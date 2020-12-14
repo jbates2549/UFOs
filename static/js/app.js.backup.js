2c7c023708e3371adb513cac8df5c6877fc7b50d
John Bates <jbatesy@yahoo.com>
2020-12-13 23:36:15 -0500

    challenge added

--- static/js/app.js.backup.js
+++ static/js/app.js.backup.js
@@ -2,6 +2,7 @@
 const tableData = data;
 // Reference the HTML table using d3
 var tbody = d3.select("tbody");
+
 function buildTable(data) {
     // First, clear out any existing data
     tbody.html("");
@@ -21,27 +22,33 @@ function buildTable(data) {
       );
     });
   }
+
   function handleClick() {
     // Grab the datetime value from the filter
     let date = d3.select("#datetime").property("value");
     let filteredData = tableData;
-  
-     // Check to see if a date was entered and filter the
-    // data using that date.
+
     if (date) {
       // Apply `filter` to the table data to only keep the
       // rows where the `datetime` value matches the filter value
       filteredData = filteredData.filter(row => row.datetime === date);
     }
+
+    buildTable(filteredData);
+  }
+    // Attach an event to listen for the form button
+    d3.selectAll("#filter-btn").on("click", handleClick);
+    // Build the table when the page loads
+    
+     // Check to see if a date was entered and filter the
+    // data using that date.
+
   
      // Rebuild the table using the filtered data
     // @NOTE: If no date was entered, then filteredData will
     // just be the original tableData.
-    buildTable(filteredData);
-  }
+    buildTable(tableData);
   
-  // Attach an event to listen for the form button
-  d3.selectAll("#filter-btn").on("click", handleClick);
   
-  // Build the table when the page loads
-  buildTable(tableData);
+  
+  
